66cb73cd08bc561bd51e52969c98e0983a1b0f8b
Kushal Baranwal <imkushal01@gmail.com>
2026-03-11 14:50:18 -0700

    added explanation of the project page

---
+++ site/app/explanation/page.tsx
@@ -0,0 +1,29 @@
+export default function Explanation() {
+    return (
+        <div className="flex min-h-screen items-center justify-center p-7 bg-black font-sans">
+            <p>
+                This is an ecosystem simulation of animals! <br />
+                Each species of animal gets its own color and its own script (i.e. how it chooses to eat, reproduce, move,
+                etc) <br />
+                Each species of animal can use information from a few factors (such as their age, energy, nearby food, etc)
+                to make decisions about what action to take
+                <br />
+                Feel free to make your own animal{" "}
+                <a className="text-blue-400" href="/submit">
+                    here.
+                </a>
+                <br />
+                You do currently need to program your own script in TypeScript, but for sufficiently simple scripts you can
+                probably get an LLM to make what you want
+                <br />
+                Each frame (currently set to 1 frame / second), the cells update to reflect the animal's actions
+                <br />
+                The code is{" "}
+                <a className="text-blue-400" href="https://github.com/kbwal/ecosystem-simulation" target="_blank">
+                    open-source
+                </a>
+                , feel free to contribute!
+            </p>
+        </div>
+    );
+}
--- site/app/page.tsx
+++ site/app/page.tsx
@@ -2,13 +2,20 @@ import Grid from "@/components/Grid";
 
 export default function Home() {
     return (
-        <div className="flex min-h-screen items-center justify-center bg-black font-sans">
-            <Grid cellSize={4} />
+        <div className="flex min-h-screen min-w-screen items-center justify-center bg-black font-sans">
+            <Grid cellSize={3} />
             <a
                 href="/submit"
-                className="fixed bottom-6 right-6 rounded-full border border-zinc-700 bg-zinc-900/80 px-4 py-2 text-sm text-zinc-300 backdrop-blur-sm transition-colors hover:border-zinc-500 hover:text-white"
+                className="fixed bottom-6 right-6 max-h-10 rounded-full border border-zinc-700 bg-zinc-900/80 px-4 py-2 text-sm text-zinc-300 backdrop-blur-sm transition-colors hover:border-zinc-500 hover:text-white"
             >
                 Submit your own animal!
+            </a>
+            <br />
+            <a
+                href="/explanation"
+                className="fixed bottom-18 right-6 max-h-10 rounded-full border border-zinc-700 bg-zinc-900/80 px-4 py-2 text-sm text-zinc-300 backdrop-blur-sm transition-colors hover:border-zinc-500 hover:text-white"
+            >
+                What's this?
             </a>
         </div>
     );
