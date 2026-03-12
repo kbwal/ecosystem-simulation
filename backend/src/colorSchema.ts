3a930e47322bfda48c928ba7906f0e49a153eafd
Kushal Baranwal <imkushal01@gmail.com>
2026-03-12 16:08:16 -0700

    fixed bug in parsing color schema

--- backend/src/colorSchema.ts
+++ backend/src/colorSchema.ts
@@ -1,9 +1,9 @@
-import { number, z } from "zod";
+import { z } from "zod";
 
 export const colorSchema = z
     .string()
-    .regex(/^\d{1,3},\d{1,3},\d{1,3}$/, "Format should be rgb!")
+    .regex(/^\d{1,3},\d{1,3},\d{1,3}$/)
     .refine((val) => {
-        const parts = val.split(",").map(number);
-        return parts.every((n) => n.lte(255) && n.gte(0));
+        const parts = val.split(",").map(Number);
+        return parts.every((n) => n >= 0 && n <= 255);
     });
--- site/utils/submissionSchemas.ts
+++ site/utils/submissionSchemas.ts
@@ -1,4 +1,4 @@
-import { z, number } from "zod";
+import { z } from "zod";
 
 export const colorSchema = z
     .string()
@@ -6,8 +6,8 @@ export const colorSchema = z
     .max(11)
     .regex(/^\d{1,3},\d{1,3},\d{1,3}$/, "Format should be rgb!")
     .refine((val) => {
-        const parts = val.split(",").map(number);
-        return parts.every((n) => n.lte(255) && n.gte(0));
+        const parts = val.split(",").map(Number);
+        return parts.every((n) => n >= 0 && n <= 255);
     });
 
 export const scriptSchema = z
